0adb4b952dfc1d71b2d7301cc4a77515d5a23c02
aamiryaqoob <aamiryaqoob965@gmail.com>
2024-07-11 23:36:35 +0500

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
-        
+
         <header className="shadow sticky z-50 top-0">
             <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
@@ -72,7 +72,6 @@ export default function Header() {
                                     Github
                                 </NavLink>
                             </li>
-
                         </ul>
                     </div>
                 </div>
